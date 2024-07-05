686b39ce4ba6b880f8002c9c28ae9026b661fc30
Lilly Lucin <lucin@duck.com>
2024-07-05 15:28:23 -0500

    Change build release script to point to my github

--- release/build_release.sh
+++ release/build_release.sh
@@ -25,7 +25,7 @@ rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 git init
-git remote add origin git@github.com:commaai/openpilot.git
+git remote add origin git@github.com:lucin678/openpilot.git
 git checkout --orphan $RELEASE_BRANCH
 
 # do the files copy
